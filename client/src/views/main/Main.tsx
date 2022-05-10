009d8f0450a8ae381be2cc9684bcdede1c917c21
CarinWood <carin.wood.85@gmail.com>
2022-05-10 13:17:37 +0200

    work on main

---
+++ client/src/views/main/Main.tsx
@@ -0,0 +1,24 @@
+import {useState} from 'react'
+
+const Main = () => {
+    const [text, setText] = useSatete('')
+
+    const postMessageFunc = () => {
+
+    }
+
+  return (
+    <div>
+        <input
+          placeholder="write something..."
+          value={text}
+          onChange={e => setText(e.target.value)}
+          type="text"
+        />
+        <button onClick={() => postMessageFunc()}>POST</button>
+
+    </div>
+  )
+}
+
+export default Main
