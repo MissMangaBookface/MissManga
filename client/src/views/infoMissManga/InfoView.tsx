931f98c8e7e7c3f6ee1cecc99db4e88c152dece7
Ursula Vallejo Janne <ursulavallejo@gmail.com>
2022-05-18 14:31:21 +0200

    template info

--- client/src/views/infoMissManga/InfoView.tsx
+++ client/src/views/infoMissManga/InfoView.tsx
@@ -1,13 +1,27 @@
-import css from './ContactView.module.css'
+import css from './InfoView.module.css'
+import contactPhoto from "../../img/postcontact.avif";
 
 const InfoView = () => {
 
     return (
-        <div >
-            <h1>INFO</h1>
+        < >
+            <header className={css.heroInfo}/>
+            <div className={css.content}>
+
+                <h1>Here you have lite information of Miss Manga Bookface!! </h1>
+
+                <div className={css.container}>
+                    <div className={css.contactForm}>
+                       <h1>We create this space for you!</h1>
+                        <p>We believe that is important to have a space where you can find friends with same interests! </p>
+                        <p>That's why Miss Manga only accept female members with same interest in MANGA culture!</p>
+
+                    </div>
+                </div>
+            </div>
 
 
-        </div>
+        </>
 
     )
 }
