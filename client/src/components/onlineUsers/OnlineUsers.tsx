b1f0acbc3c58ff8558ec1c76ada806b7cde2d07d
Carin Wood <carin.wood.85@gmail.com>
2022-05-24 15:58:03 +0200

    update responsive

--- client/src/components/onlineUsers/OnlineUsers.tsx
+++ client/src/components/onlineUsers/OnlineUsers.tsx
@@ -20,7 +20,7 @@ const OnlineUsers:FC<Props> = ({getOnlineUsers, onlineUsers}) => {
     
   return (
     <div className='online-users-container'>
-        <h2 data-testid='onlineUser'>  Online Users: </h2>
+        <h2 className='online-user-heading' data-testid='onlineUser'>  Online Users: </h2>
         <div>
             {onlineUsers.map(user => (
                 <div  className='online-username-div'>
