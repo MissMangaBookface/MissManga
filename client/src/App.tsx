8bd8c8272d5eff729941a76aeae287915699c1db
Carin Wood <carin.wood.85@gmail.com>
2022-05-31 12:23:31 +0200

    header fixed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import LoginPage from './components/loginPage/LoginPage';
 import Routing from "./routing/Routing";
-
+import Header from './components/header/Header';
 import Footer from "./components/footer/Footer";
 import NavBar from "./components/navbar/NavBar";
 
@@ -13,7 +13,7 @@ function App() {
         <h1 className='headline'>Miss <span className='manga'>Manga</span>Bookface</h1>
         <Routing>
         </Routing>
-        <NavBar/>
+        <Header />
         <Footer/>
     </div>
    
