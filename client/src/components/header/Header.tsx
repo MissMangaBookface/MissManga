eebf900fe0a41fa3ea279fb372036f39cadd0b8d
Carin Wood <carin.wood.85@gmail.com>
2022-05-31 10:43:19 +0200

    header

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,22 +1,36 @@
-import React, { FC, useState, useEffect } from 'react'
+import React, { FC } from 'react'
 import './header.css'
+import {Link} from 'react-router-dom'
+import RoutingPath from "../../routing/RoutingPath";
+import css from '../navbar/Navbar.module.css'
 
-interface Props {
-  getOnlineUsers: () => void
-}
 
-const Header:FC<Props> = ({getOnlineUsers}) => {
-    const [username, setUsername] = useState<string | null>('')
 
-    useEffect(() => {
-        setUsername(localStorage.getItem("username"))    
-    }, [getOnlineUsers])
-
-  
+const Header:FC = () => {
+ 
 
   return (
     <div className='header'>
-        <h2 className='header-name'>Hi {username}!</h2>
+      <nav className="pv4 ph3 ph5-ns tc">
+            <ul className={css.navList}>
+                <li className={css.navItem}>
+                    <Link to={RoutingPath.infoView}>InfoMissManga</Link>
+                </li>
+                <li className={css.navItem} >
+                    <Link  to={RoutingPath.contactView}>Contact</Link>
+                </li>
+                <li className={css.navItem}>
+                    <Link  to={RoutingPath.aboutView}>About us</Link>
+                </li>
+
+              {/*ternary*/}
+                    <li className={css.navItem}>
+                        <Link  to={RoutingPath.mainView}>My page</Link></li>
+
+                    <li className={css.navItem}>
+                        <Link to={RoutingPath.loginView}>Log In</Link></li>
+            </ul>
+        </nav>
       
     </div>
   )
