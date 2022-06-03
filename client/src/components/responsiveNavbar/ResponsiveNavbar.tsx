c27867ce071d056b6a7653840b971f6d3a22520d
Carin Wood <carin.wood.85@gmail.com>
2022-06-03 17:58:45 +0200

    commit

--- client/src/components/responsiveNavbar/ResponsiveNavbar.tsx
+++ client/src/components/responsiveNavbar/ResponsiveNavbar.tsx
@@ -1,6 +1,8 @@
 import {useState} from 'react'
 import './responsiveNavbar.css'
 import { GrMenu } from 'react-icons/gr';
+import {Link} from 'react-router-dom'
+import RoutingPath from '../../routing/RoutingPath';
 
 const ResponsiveNavbar = () => {
         const [toggleMenu, setToggleMenu] = useState(false)
@@ -9,16 +11,18 @@ const toggleMenuFunc = () => {
     setToggleMenu(!toggleMenu)
 }
 
+
+
   return (
-    <div>
+    <div className={toggleMenu ? 'responsive-menu show': 'responsive-menu'}>
         <GrMenu className='menu-icon' onClick={() => toggleMenuFunc()}/>
 
-        <ul className={toggleMenu ? 'responsive-menu show': 'responsive-menu'}>
-            <li className='responsive-list-item'>InfoMissManga</li>
-            <li className='responsive-list-item'>Contact</li>
-            <li className='responsive-list-item'>About Us</li>
-            <li className='responsive-list-item'>My Page</li>
-            <li className='responsive-list-item'>Log in</li>
+        <ul className="responsive-ul">
+        <Link className='responsive-list-item' to={RoutingPath.infoView} onClick={() =>toggleMenuFunc()}><li> InfoMissManga</li></Link>
+        <Link className='responsive-list-item' to={RoutingPath.contactView} onClick={() =>toggleMenuFunc()}><li> Contact</li></Link>
+        <Link className='responsive-list-item' to={RoutingPath.aboutView} onClick={() =>toggleMenuFunc()}><li >About us</li></Link>
+        <Link className='responsive-list-item' to={RoutingPath.mainView} onClick={() =>toggleMenuFunc()}><li>My page</li></Link>
+        <Link className='responsive-list-item' to={RoutingPath.loginView} onClick={() =>toggleMenuFunc()}><li >Log In</li></Link>
           
         </ul>
 
